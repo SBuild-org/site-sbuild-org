d1609a57435be817f887265d1727db76bf416b91
Tobias Roeser <le.petit.fou@web.de>
2015-09-10 09:24:28 +0200

    Fixed eval of TRAVIS_PULL_REQUEST variable

--- build-and-deploy-to-github.sh
+++ build-and-deploy-to-github.sh
@@ -11,7 +11,7 @@ LC_ALL=C ./sbuild-wrapper jbake
 
 echo "sbuild.org" > target/CNAME
 
-if [ -z "$TRAVIS_PULL_REQUEST}" -a "${TRAVIS_BRANCH}" == "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then
 
   echo "Deploying to GitHub pages branch"
 
