2a040c2ad176ace805c6952dad387c5888ede8f9
Tobias Roeser <le.petit.fou@web.de>
2016-09-29 09:14:12 +0200

    Updated build script

--- build-and-deploy-to-github.sh
+++ build-and-deploy-to-github.sh
@@ -1,14 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env sh -x
 
 set -e
 
 echo "TRAVIS_BRANCH=${TRAVIS_BRANCH}"
 echo "TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST}"
 
+echo ">>> Fetching assets"
+
 git clone --single-branch --branch master "https://github.com/SBuild-org/site-sbuild-org-assets-1" assets-repo
 
 mkdir target
 cp -r assets-repo/assets/uploads target/uploads
+
+echo ">>> Building site"
 
 LC_ALL=C ./sbuild-wrapper jbake
 
@@ -16,7 +20,7 @@ echo "sbuild.org" > target/CNAME
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then
 
-  echo "Deploying to GitHub pages branch"
+  echo ">>> Preparing GitHub pages branch"
 
   cd target
 
@@ -29,9 +33,13 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then
 
   git commit -m "Automatic deploy to GitHub Pages."
 
+  echo ">>> Deploying GitHub pages branch"
+
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
 
 else
-  echo "Skipping deployment to GitHub pages as this is not the master branch or a pull request"
+  echo ">>> Skipping deployment to GitHub pages as this is not the master branch or a pull request"
 
 fi
+
+echo "<<< DONE
